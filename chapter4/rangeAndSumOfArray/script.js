be041f9074537f6af43d79b5e35a7c59c3c5c7fe
David R Tarvin Jr <david.tarvin@gmail.com>
2017-05-01 07:38:06 -0500

    Chapter 4 Problem 1

---
+++ chapter4/rangeAndSumOfArray/script.js
@@ -0,0 +1,32 @@
+function range(num1, num2, step) {
+  var numArray=[];
+  if (step == null) {
+    step = 1;
+  }
+  if (num1 > num2) {
+    for (i=num1; i>=num2; i=i+step) {
+      numArray.push(i);
+    }
+  }
+  else {
+    for (i=num1; i<=num2; i=i+step) {
+      numArray.push(i);
+    }
+  }
+  return numArray;
+}
+
+function sum(numArray) {
+  sumArray = 0;
+  for (i=0; i<numArray.length; i++) {
+    sumArray = sumArray+numArray[i];
+  }
+  return sumArray;
+}
+console.log(range(1, 10));
+// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(10, 2, -2));
+// → [5, 4, 3, 2]
+console.log(sum(range(1, 10)));
+// → 55
+console.log(range(1, 10, 2));
